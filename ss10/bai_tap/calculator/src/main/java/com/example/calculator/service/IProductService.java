be376be7100c1a298026bc39b7f51dcc28d7f107
DaiHoang0910 <hoangvandai0910@gmail.com>
2024-11-29 17:05:47 +0700

    bai tap

--- ss10/bai_tap/calculator/src/main/java/com/example/calculator/service/IProductService.java
+++
@@ -1,6 +0,0 @@
-package com.example.calculator.service;
-
-import com.example.calculator.model.Product;
-
-public interface IProductService extends IService<Product>{
-}
--- ss10/bai_tap/calculator/src/main/java/com/example/calculator/service/IService.java
+++
@@ -1,7 +0,0 @@
-package com.example.calculator.service;
-
-import java.util.List;
-
-public interface IService<T> {
-    List<T> getAll();
-}
